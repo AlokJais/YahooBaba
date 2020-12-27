044341e251b591937f963ce4be068af0225ef949
Alok Jaiswal <57588918+AlokJais@users.noreply.github.com>
2020-12-27 12:19:59 +0530

    if_else.php

---
+++ yb_php/13_Logical_Operators/13_Logical_Operators.php
@@ -0,0 +1,51 @@
+<?php
+
+
+// $age= 20;
+
+// if($age>=18 && $age<=21){        // &&==and
+//     echo "You are eligible";
+// }
+
+// $age= 25;
+
+// if($age>=18 && $age<=21){
+//     echo "You are eligible";
+// }
+
+//  $age= 20;
+
+//  if($age>=18 || $age<=21){      // ||==or
+//          echo "You are eligible";  
+//  }
+
+//  $age= 25;
+
+//  if($age>=18 || $age<=21){
+//          echo "You are eligible";
+//  }
+
+
+//  $age= 25;
+//  if(!($age>=18)){
+//          echo "You are eligible";
+//  }
+
+//  $age= 25;
+//  if(!($age<=18)){
+//          echo "You are eligible";
+//  }
+
+
+//  $age= 20;
+//  if($age>=18 xor $age<=21){
+//          echo "You are eligible";
+//  }
+
+ $age= 26;
+ if($age>=18 xor $age<=21){
+         echo "You are eligible";
+ }
+
+
+?>
---
+++ yb_php/14_if_else/14_if_else.php
@@ -0,0 +1,48 @@
+<?php
+
+// $x= 15;
+// if($x > 30){
+//   echo "X is greater.";
+// }else{
+//     echo "X is smaller.";
+// }
+
+// $x= 50;
+// if($x > 30){
+//   echo "X is greater.";
+// }else{
+//     echo "X is smaller.";
+// }
+
+// $x= 100;
+// if($x == "100"){
+//   echo "X is same.";
+// }else{
+//     echo "X is not same.";
+// }
+
+// $x= 100;
+// if($x === "100"){
+//   echo "X is same.";
+// }else{
+//     echo "X is not same.";
+// }
+
+// $name= "Yahoo Baba";
+// $gender= "male";
+// if($gender == "male"){
+//   echo "Hello Mr." . $name;
+// }else{
+//     echo "Hello Miss." . $name;
+// }
+
+$name= "Yahoo Baba";
+$gender= "female";
+if($gender == "male"){
+  echo "Hello Mr." . $name;
+}else{
+    echo "Hello Miss." . $name;
+}
+
+
+?>
