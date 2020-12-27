aa07ebaf479cd25527bb9561e2b4c029b1267da0
Alok Jaiswal <57588918+AlokJais@users.noreply.github.com>
2020-12-27 11:35:52 +0530

    changes

---
+++ yb_php/12_if_statement/12_if_statement.php
@@ -0,0 +1,52 @@
+<?php
+
+// $a= 3;
+// $b= 10;
+
+// if($a < $b){
+//     echo "A is smaller than B";
+// }
+
+
+// $a= 30;
+// $b= 10;
+
+// if($a < $b){
+//     echo "A is smaller than B";
+// }
+
+// echo "Here is another statement";
+
+
+// $a= 30;
+// $b= 30;
+
+// if($a == $b){
+//     echo "A is smaller than B <br>";
+// }
+
+// echo "Here is another statement";
+
+
+// $a= 30;
+// $b= "30";
+
+// if($a === $b){
+//     echo "A is smaller than B <br>";
+// }
+
+// echo "Here is another statement";
+
+
+$a= 30;
+$b= "30";
+
+if($a == $b):
+    echo "A is smaller than B <br>";
+    echo "A is smaller than B <br>";
+endif;
+
+echo "Here is another statement";
+
+
+ ?>
