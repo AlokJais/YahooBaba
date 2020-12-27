3ffdd717d4a64cf92d087ce9a2c809e3af1514ef
Alok Jaiswal <57588918+AlokJais@users.noreply.github.com>
2020-12-27 15:26:37 +0530

    switch case

---
+++ yb_php/16_Switch_Case/16_switch_case.php
@@ -0,0 +1,99 @@
+<?php
+$weekday= 9;
+switch($weekday){
+case 1:
+    echo "Today is Monday";
+    echo "Today is just test.";
+    break;
+case 2:
+    echo "Today is Tuesday";
+    break;
+case 3:
+    echo "Today is Wednesday";
+    break;
+case 4:
+    echo "Today is Thursday";
+    break;
+case 5:
+    echo "Today is Friday";
+    break;
+case 6:
+    echo "Today is Saturday";
+    break;
+case 7:
+    echo "Today is Sunday";
+    break;
+default:
+    echo "Enter the valid Weekday";
+}
+
+
+
+// $age= 17;
+//  switch(true){
+//      case ($age>=15 && $age<=20):
+//         echo "You are elligible";
+//         break;
+//      case ($age>=21 && $age<=30):
+//         echo "You are not elligible";
+//         break;
+//     default:
+//         echo "Enter the valid age";
+//  }
+
+
+
+// $weekday= 4;
+// switch($weekday){
+// case 1: case 2: case 3:
+//     echo "Today is Monday <br>";
+//     echo "Today is just test.";
+//     break;
+
+// case 4:
+//     echo "Today is Thursday";
+//     break;
+// case 5:
+//     echo "Today is Friday";
+//     break;
+// case 6:
+//     echo "Today is Saturday";
+//     break;
+// case 7:
+//     echo "Today is Sunday";
+//     break;
+// default:
+//     echo "Enter the valid Weekday";
+// }
+    
+
+
+// $weekday= 2;
+// switch($weekday){
+// case 1:
+//     echo "Today is Monday";
+//     echo "Today is just test.";
+    
+// case 2:
+//     echo "Today is Tuesday";
+    
+// case 3:
+//     echo "Today is Wednesday";
+    
+// case 4:
+//     echo "Today is Thursday";
+    
+// case 5:
+//     echo "Today is Friday";
+    
+// case 6:
+//     echo "Today is Saturday";
+//     break;
+// case 7:
+//     echo "Today is Sunday";
+//     break;
+// default:
+//     echo "Enter the valid Weekday";
+    
+// }
+?>
