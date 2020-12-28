cb3b5e97bfaf47161581271e7b561731536a0670
Alok Jaiswal <57588918+AlokJais@users.noreply.github.com>
2020-12-28 09:01:55 +0530

    ternary_operator

---
+++ yb_php/17_Ternary_Operator/17_ternary.php
@@ -0,0 +1,22 @@
+<?php
+$x= 10;
+($x > 20)? $z="Greater" : $z="Smaller";
+echo $z;
+
+
+// $x= 30;
+// $z =($x > 20)? "Greater" : "Smaller";
+// echo $z;
+
+
+// $x= 30;
+// $z =$x > 20? "Greater" : "Smaller";
+// echo $z;
+
+
+// $x= 30;
+// $z = "Value is : " .($x > 20? "Greater" : "Smaller");
+// echo $z;
+
+
+?>
---
+++ yb_php/18_string_operator.php/18_string_operator.php
@@ -0,0 +1,21 @@
+<?php
+
+// $a= "Hello ";
+// $s= $a . "World";
+// echo $s;
+
+
+// $a= "200 ";
+// $s= $a . "World " . 500;
+// echo $s;
+
+
+$s= "Hello ";
+$s.= "this is ";
+$s.= 555;
+$s.= "our world";
+echo $s;
+
+
+
+?>
